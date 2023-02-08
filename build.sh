bc627c8def64a5e44545fb8f741f59818d8a9965
Gonçalo Xavier <goncalox11@gmail.com>
2023-02-08 14:03:08 +0000

    Change executable name in build script

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ then
     c++ -O2  -g3 -I Includes -I ./ backends/imgui_impl_glfw.cpp -c -o build/imgui_impl_glfw.o
     c++ -O2  -g3 -I Includes -I ./ backends/imgui_impl_opengl3.cpp -c -o build/imgui_impl_opengl3.o
 fi
-#c++ -I ./ -I build/ -I backends/  sorting.cpp implot/implot_demo.cpp imgui_demo.cpp build/*.o -o build/foo -g3 -lGL -lX11 -ldl -lpthread 
-c++ -I ./ -I build/ -I backends/  sorting.cpp  Includes/imgui_demo.cpp build/*.o -o build/foo -g3 -lGL -lX11 -ldl -lpthread  -Wfatal-errors
+
+c++ -I ./ -I build/ -I backends/  sorting.cpp  Includes/imgui_demo.cpp build/*.o -o build/sortingVisualizer -g3 -lGL -lX11 -ldl -lpthread  -Wfatal-errors
 
 export DISPLAY=$(ipconfig.exe | grep IPv4 | head -1 | tr -d '\r' | awk '{print $NF}'):0.0
 export LIBGL_ALWAYS_INDIRECT=1
